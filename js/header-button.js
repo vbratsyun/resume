565af055dc73143c14283614cbc7cf941e6609d9
vbratsyun <91077541+vbratsyun@users.noreply.github.com>
2025-02-14 15:06:54 +0000

    Deploying to gh-pages from @ vbratsyun/resume@e8c9c910d05d5c3fc21b4d8eef4a043f7a8cfcf8 🚀

---
+++ js/header-button.js
@@ -0,0 +1,21 @@
+const button = document.querySelector(".header__button");
+const portfolio = document.querySelector(".portfolio");
+
+let timer;
+
+const onArrowClick = () => {
+  portfolio.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "start",
+  });
+};
+
+const onWindowLoad = () => {
+  timer = setInterval(() => {
+    button.classList.toggle("header__button--transform");
+    button.addEventListener("click", onArrowClick);
+  }, 600);
+};
+
+window.addEventListener("load", onWindowLoad);
